b2c88239c6632606767c6099ec9e81401a6cc6bb
Cunuduh <nickleodean.email3@gmail.com>
2023-10-30 20:01:45 -0400

    Rewrite in SvelteKit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Club-Website
+# Club Website
+Here are the instructions for developing the club website locally:
+```bash
+# install pnpm and typescript globally
+npm i -g pnpm typescript
+# install dependencies
+pnpm i
+# start the server
+npm run dev
+
+# or start the server and open the app in a new browser tab
+pnpm run dev -- --open
+```
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Nav from '../../components/Nav.svelte';
+</script>
+<Nav />
+<main>
+  <p>about page</p>
+  <section id="content">
+    <div>
+      <h2>Our Team</h2>
+      <div class="wrapper">
+        <p>Going to put memebers of the executive here</p>
+      </div>
+    </div>
+    <div>
+      <h2>What We Do</h2>
+      <div class="wrapper">
+        <p>stuff</p>
+      </div>
+    </div>
+  </section>
+</main>
