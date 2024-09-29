2409eb3ab690967aa6b506241d510abe8b94feae
archiso <awesomelax7@gmail.com>
2024-09-29 17:42:26 -0400

    fix url issues

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-fetch('/header.html')
+fetch('header.html')
             .then(response => response.text())
             .then(data => {
                 document.getElementById('header').innerHTML = data;
